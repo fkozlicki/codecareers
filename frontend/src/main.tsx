e00a1018ef1bbb09661167598de84b9e1eee6894
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-05 17:22:28 +0100

    Added new pages to router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,12 @@ import SignIn from './pages/sign-in.tsx';
 import SignUp from './pages/sign-up.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
+import SessionProvider from './components/session-provider.tsx';
+import Settings from './pages/settings.tsx';
+import ProtectedRoute from './components/protected-route.tsx';
+import Jobs from './pages/jobs.tsx';
+import Companies from './pages/companies.tsx';
+import CreateCompany from './pages/create-company.tsx';
 
 const router = createBrowserRouter([
 	{
@@ -19,6 +25,38 @@ const router = createBrowserRouter([
 			{
 				path: '/',
 				element: <Home />,
+			},
+			{
+				path: '/settings',
+				element: (
+					<ProtectedRoute>
+						<Settings />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/my-jobs',
+				element: (
+					<ProtectedRoute>
+						<Jobs />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/my-companies',
+				element: (
+					<ProtectedRoute>
+						<Companies />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: '/my-companies/create',
+				element: (
+					<ProtectedRoute>
+						<CreateCompany />
+					</ProtectedRoute>
+				),
 			},
 		],
 	},
@@ -35,7 +73,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<SessionProvider>
+				<RouterProvider router={router} />
+			</SessionProvider>
 		</Provider>
 	</React.StrictMode>
 );
