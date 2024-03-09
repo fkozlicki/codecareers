7b59b1b77d3db69d646b6d66e41d89aab862a0af
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-09 15:56:28 +0100

    Removed Link from job-offer-card

--- frontend/src/components/job-offer-card.tsx
+++ frontend/src/components/job-offer-card.tsx
@@ -1,17 +1,13 @@
 import { JobOffer } from '@/app/services/jobOffers';
-import { Card, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
+import { Card, CardTitle } from '@/components/ui/card';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { Link, useParams } from 'react-router-dom';
 
 dayjs.extend(relativeTime);
 
 const JobOfferCard = ({ jobOffer }: { jobOffer: JobOffer }) => {
-	const { id } = useParams();
-
 	const {
-		id: jobOfferId,
 		position,
 		salaryFrom,
 		salaryTo,
@@ -23,26 +19,22 @@ const JobOfferCard = ({ jobOffer }: { jobOffer: JobOffer }) => {
 	} = jobOffer;
 
 	return (
-		<Link to={`/my-companies/${id}/job-offers/${jobOfferId}`}>
-			<Card className="p-4 hover:shadow-md">
-				<div className="flex items-center justify-between mb-4">
-					<CardTitle>{position}</CardTitle>
-					<span>
-						{salaryFrom} - {salaryTo} {salaryCurrency}
-					</span>
+		<Card className="p-4 hover:shadow-md">
+			<div className="flex items-center justify-between mb-4">
+				<CardTitle>{position}</CardTitle>
+				<span>
+					{salaryFrom} - {salaryTo} {salaryCurrency}
+				</span>
+			</div>
+			<div className="flex justify-between items-center">
+				<div className="flex gap-2">
+					<Badge variant="outline">{level}</Badge>
+					<Badge variant="outline">{workType}</Badge>
+					<Badge variant="outline">{employmentType}</Badge>
 				</div>
-				<div className="flex justify-between items-center">
-					<div className="flex gap-2">
-						<Badge variant="outline">{level}</Badge>
-						<Badge variant="outline">{workType}</Badge>
-						<Badge variant="outline">{employmentType}</Badge>
-					</div>
-					<span className="text-xs opacity-50">
-						{dayjs(createdAt).fromNow()}
-					</span>
-				</div>
-			</Card>
-		</Link>
+				<span className="text-xs opacity-50">{dayjs(createdAt).fromNow()}</span>
+			</div>
+		</Card>
 	);
 };
 
