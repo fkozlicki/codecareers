bc77039e6806b04a5e8a84dfca6d4bfcd5aaf394
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-09 15:56:46 +0100

    Created job-offer-list

---
+++ frontend/src/components/job-offer-list.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import JobOfferCard from './job-offer-card';
+import { useGetJobOffersQuery } from '@/app/services/jobOffers';
+
+const JobOfferList = () => {
+	const { data } = useGetJobOffersQuery();
+
+	return (
+		<div className="flex-1 p-4 flex flex-col gap-4">
+			{data?.jobOffers.map((jobOffer) => (
+				<Link key={jobOffer.id} to={`/?joid=${jobOffer.id}`}>
+					<JobOfferCard jobOffer={jobOffer} />
+				</Link>
+			))}
+		</div>
+	);
+};
+
+export default JobOfferList;
