0c3e63a131272d62fb02faee6991ff3fa0c0acea
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-12 17:02:15 +0100

    Added update route to applications

--- backend/src/routes/applications.ts
+++ backend/src/routes/applications.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { requireSession } from '../middleware/session';
-import { getApplications } from '../controllers/applicationsController';
+import {
+	getApplications,
+	updateApplication,
+} from '../controllers/applicationsController';
 
 export const applicationsRouter = Router();
 
 applicationsRouter.route('/').get(requireSession, getApplications);
+applicationsRouter.route('/:id').put(requireSession, updateApplication);
