8ecada563953916c91ecf2cec0e4aa45e3f9c946
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-13 12:06:02 +0100

    Added heading to applications-layout

--- frontend/src/pages/applications-layout.tsx
+++ frontend/src/pages/applications-layout.tsx
@@ -7,6 +7,9 @@ const ApplicationsLayout = () => {
 
 	return (
 		<div className="max-w-2xl m-auto py-8">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-4xl text-center mb-4">
+				Your applications
+			</h1>
 			<Tabs
 				value={sort ?? 'pending'}
 				defaultValue="pending"
