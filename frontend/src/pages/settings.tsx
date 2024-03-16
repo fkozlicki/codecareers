b2f778347a25c6b7ec14e05f763d948bf4dd1074
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-16 14:21:21 +0100

    Added image upload to settings

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -1,4 +1,16 @@
 import { useAppSelector } from '@/app/hooks';
+import { useUpdateUserMutation } from '@/app/services/users';
+import ImageCropper from '@/components/image-cropper';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from '@/components/ui/dialog';
+import { Dropzone } from '@/components/ui/dropzone';
 import {
 	Form,
 	FormControl,
@@ -9,13 +21,16 @@ import {
 	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { readFile } from '@/lib/cropImage';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 const formSchema = z.object({
 	firstName: z.string().min(1),
 	lastName: z.string().min(1),
+	avatar: z.instanceof(Blob).optional(),
 });
 
 const Settings = () => {
@@ -27,9 +42,19 @@ const Settings = () => {
 			lastName: user?.lastName ?? '',
 		},
 	});
+	const [image, setImage] = useState<string | null>(null);
+	const [cropperOpen, setCropperOpen] = useState<boolean>(false);
+	const [updateUser] = useUpdateUserMutation();
+
+	const avatar = form.watch('avatar');
+	const avatarUrl = avatar ? URL.createObjectURL(avatar) : undefined;
 
 	const onSubmit = (values: z.infer<typeof formSchema>) => {
-		console.log(values);
+		updateUser({ id: user!.id, ...values })
+			.unwrap()
+			.then(() => {
+				window.location.reload();
+			});
 	};
 
 	return (
@@ -39,7 +64,10 @@ const Settings = () => {
 					Hi, {user?.username || `${user?.firstName} ${user?.lastName}`}
 				</h1>
 				<Form {...form}>
-					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<form
+						onSubmit={form.handleSubmit(onSubmit, console.log)}
+						className="space-y-8"
+					>
 						<FormField
 							control={form.control}
 							name="firstName"
@@ -72,6 +100,51 @@ const Settings = () => {
 								</FormItem>
 							)}
 						/>
+						<div className="flex items-start gap-4">
+							<Avatar>
+								{user?.avatar && (
+									<AvatarImage src={avatarUrl || user.avatar} alt="avatar" />
+								)}
+							</Avatar>
+							<Dropzone
+								className="flex-1"
+								onChange={async (file) => {
+									const imageUrl = await readFile(file);
+									setImage(imageUrl as string);
+									setCropperOpen(true);
+								}}
+								accept="image/*"
+							/>
+						</div>
+						{image && (
+							<Dialog
+								open={cropperOpen}
+								onOpenChange={(open) => {
+									if (!open) {
+										setImage(null);
+									}
+									setCropperOpen(open);
+								}}
+							>
+								<DialogContent>
+									<DialogHeader>
+										<DialogTitle>Crop your avatar</DialogTitle>
+										<DialogDescription>
+											Make changes to your profile here. Click save when you're
+											done.
+										</DialogDescription>
+									</DialogHeader>
+									<ImageCropper
+										image={image}
+										setResult={(file) => {
+											form.setValue('avatar', file);
+											setCropperOpen(false);
+										}}
+									/>
+								</DialogContent>
+							</Dialog>
+						)}
+						<Button type="submit">Submit</Button>
 					</form>
 				</Form>
 			</div>
