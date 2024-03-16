e6a3efe5cd86922bad47c3f1966b7ecaa1a1bfc4
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-16 14:20:54 +0100

    Adjusted dropzone component

--- frontend/src/components/ui/dropzone.tsx
+++ frontend/src/components/ui/dropzone.tsx
@@ -1,34 +1,28 @@
 import { Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import {
-	ChangeEvent,
-	Dispatch,
-	DragEvent,
-	SetStateAction,
-	useRef,
-	useState,
-} from 'react';
+import { cn } from '@/lib/utils';
+import { ChangeEvent, DragEvent, useRef, useState } from 'react';
 
 interface DropzoneProps {
-	onChange: Dispatch<SetStateAction<File>>;
+	onChange: (file: File) => void;
 	className?: string;
-	fileExtension?: string;
+	accept?: string;
 }
 
 export const Dropzone = ({
 	onChange,
 	className,
-	fileExtension,
+	accept,
 	...props
 }: DropzoneProps) => {
 	const fileInputRef = useRef<HTMLInputElement | null>(null); // Reference to file input element
 	const [fileInfo, setFileInfo] = useState<string | null>(null); // Information about the uploaded file
-	const [error, setError] = useState<string | null>(null); // Error message state
+	const [isOver, setIsOver] = useState<boolean>(false);
 
 	// Function to handle drag over event
 	const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
 		e.stopPropagation();
+		setIsOver(true);
 	};
 
 	// Function to handle drop event
@@ -37,6 +31,7 @@ export const Dropzone = ({
 		e.stopPropagation();
 		const { files } = e.dataTransfer;
 		handleFiles(files);
+		setIsOver(false);
 	};
 
 	// Function to handle file input change event
@@ -45,26 +40,19 @@ export const Dropzone = ({
 		if (files) {
 			handleFiles(files);
 		}
+		e.target.value = '';
 	};
 
 	// Function to handle processing of uploaded files
 	const handleFiles = (files: FileList) => {
 		const uploadedFile = files[0];
 
-		// Check file extension
-		if (fileExtension && !uploadedFile.name.endsWith(`.${fileExtension}`)) {
-			setError(`Invalid file type. Expected: .${fileExtension}`);
-			return;
-		}
-
 		const fileSizeInKB = Math.round(uploadedFile.size / 1024); // Convert to KB
 
-		// const fileList = Array.from(files).map((file) => URL.createObjectURL(file));
 		onChange(uploadedFile);
 
 		// Display file information
 		setFileInfo(`Uploaded file: ${uploadedFile.name} (${fileSizeInKB} KB)`);
-		setError(null); // Reset error state
 	};
 
 	// Function to simulate a click on the file input element
@@ -76,36 +64,33 @@ export const Dropzone = ({
 
 	return (
 		<Card
-			className={`border-2 border-dashed bg-muted hover:cursor-pointer hover:border-muted-foreground/50 ${className}`}
+			onClick={handleButtonClick}
+			className={cn(
+				'border-2 border-dashed bg-muted hover:cursor-pointer hover:border-muted-foreground/50',
+				{ 'border-muted-foreground/50': isOver },
+				className
+			)}
 			{...props}
 		>
 			<CardContent
 				className="flex flex-col items-center justify-center space-y-2 px-2 py-4 text-xs"
 				onDragOver={handleDragOver}
 				onDrop={handleDrop}
+				onDragLeave={() => setIsOver(false)}
 			>
 				<div className="flex items-center justify-center text-muted-foreground">
-					<span className="font-medium">Drag Files to Upload or</span>
-					<Button
-						type="button"
-						variant="ghost"
-						size="sm"
-						className="ml-auto flex h-8 space-x-2 px-0 pl-1 text-xs"
-						onClick={handleButtonClick}
-					>
-						Click Here
-					</Button>
+					<span className="font-medium">
+						{isOver ? 'Drop Here' : 'Drag Files or Click Here to Upload'}
+					</span>
 					<input
 						ref={fileInputRef}
 						type="file"
-						accept={`.${fileExtension}`} // Set accepted file type
+						accept={accept}
 						onChange={handleFileInputChange}
 						className="hidden"
-						multiple
 					/>
 				</div>
 				{fileInfo && <p className="text-muted-foreground">{fileInfo}</p>}
-				{error && <span className="text-red-500">{error}</span>}
 			</CardContent>
 		</Card>
 	);
