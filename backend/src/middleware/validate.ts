036ca2d04de6c33838062559ffe46493467bf515
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-03-22 20:20:13 +0100

    Adjusted validate middleware

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -14,10 +14,7 @@ export const validate =
 			return next();
 		} catch (error) {
 			if (error instanceof ZodError) {
-				const errorMessages = error.errors.map((issue) => ({
-					message: `${issue.path.join('.')} is ${issue.message}`,
-				}));
-				res.status(400).json({ error: 'Invalid data', details: errorMessages });
+				res.status(400).json({ error: 'Invalid data' });
 			} else {
 				res.status(500).json({ error: 'Internal Server Error' });
 			}
