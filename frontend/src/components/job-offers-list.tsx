36dab4cf66f630a4b0c66b1a3330d1d986c16e33
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-04-08 22:23:45 +0200

    Adjusted infinite scroll

--- frontend/src/components/job-offers-list.tsx
+++ frontend/src/components/job-offers-list.tsx
@@ -1,4 +1,7 @@
-import { useLazyGetJobOffersQuery } from '@/app/services/jobOffers';
+import {
+	useGetJobOffersQuery,
+	useLazyGetJobOffersQuery,
+} from '@/app/services/jobOffers';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Link, useSearchParams } from 'react-router-dom';
@@ -6,19 +9,21 @@ import JobOfferCard from './job-offer-card';
 import JobOfferSkeleton from './job-offer-skeleton';
 
 const JobOffersList = () => {
-	const [fetchJobOffers, { data, isLoading, isFetching, isUninitialized }] =
-		useLazyGetJobOffersQuery();
 	const [searchParams] = useSearchParams();
-	const joid = searchParams.get('joid');
 	const name = searchParams.get('name');
+	const { data, isLoading, isFetching, isUninitialized } = useGetJobOffersQuery(
+		{ pageSize: 2, name }
+	);
+	const [fetchJobOffers] = useLazyGetJobOffersQuery();
 	const [ref, inView] = useInView();
+	const joid = searchParams.get('joid');
 	const cursor = data?.jobOffers.at(-1)?.id;
 
 	useEffect(() => {
-		if (inView) {
-			fetchJobOffers({ pageSize: 10, cursor, name });
+		if (inView && !isLoading) {
+			fetchJobOffers({ pageSize: 2, cursor, name });
 		}
-	}, [inView, cursor, name]);
+	}, [inView, cursor]);
 
 	if (isLoading || isUninitialized) {
 		return (
