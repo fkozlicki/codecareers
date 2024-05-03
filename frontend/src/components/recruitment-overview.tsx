91164096ae43210ded1280510a0d4d17cef055cf
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-05-03 14:27:02 +0200

    Adjusted recruitment-overview component

--- frontend/src/components/recruitment-overview.tsx
+++ frontend/src/components/recruitment-overview.tsx
@@ -1,8 +1,12 @@
 import { useGetRecruitmentQuery } from '@/app/services/recruitments';
-import { User } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import dayjs from 'dayjs';
+import { FilePenIcon, FileTextIcon, User, UserIcon } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 import PDFViewer from './pdf-viewer';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Badge } from './ui/badge';
+import { Separator } from './ui/separator';
 import { Skeleton } from './ui/skeleton';
 
 const RecruitmentOverview = () => {
@@ -26,10 +30,28 @@ const RecruitmentOverview = () => {
 		return <div>Couldn't load data</div>;
 	}
 
-	const { user, jobOffer, cv } = data.recruitment.application;
+	const { createdAt, open } = data.recruitment;
+	const { user, jobOffer, cv, introduction } = data.recruitment.application;
 
 	return (
 		<div>
+			<h2 className="inline-flex items-center text-xl mb-4">
+				{jobOffer.position}
+			</h2>
+			<div className="flex justify-between items-center mb-4">
+				<Badge
+					variant={open ? 'outline' : 'destructive'}
+					className={cn({ 'border-green-500 text-green-500': open })}
+				>
+					{open ? 'Open' : 'Closed'}
+				</Badge>
+				<span>Started {dayjs(createdAt).fromNow()}</span>
+			</div>
+			<Separator className="my-4" />
+			<h2 className="inline-flex items-center text-xl mb-4 w-full">
+				<UserIcon size={24} className="text-gray-500 mr-2" />
+				User
+			</h2>
 			<div className="flex items-center gap-2">
 				<Avatar>
 					<AvatarFallback>
@@ -39,7 +61,17 @@ const RecruitmentOverview = () => {
 				</Avatar>
 				<span>{user.username}</span>
 			</div>
-			<span>Applying for {jobOffer.position}</span>
+			<Separator className="my-4" />
+			<h2 className="inline-flex items-center text-xl mb-4">
+				<FilePenIcon size={24} className="text-gray-500 mr-2" />
+				Introduction
+			</h2>
+			<p>{introduction}</p>
+			<Separator className="my-4" />
+			<h2 className="inline-flex items-center text-xl mb-4">
+				<FileTextIcon size={24} className="text-gray-500 mr-2" />
+				CV
+			</h2>
 			<PDFViewer href={`${import.meta.env.VITE_API_URI}/cv/${cv}`} />
 		</div>
 	);
