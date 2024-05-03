97e3a7eaae25fbbc9efc5b0badec44b7fa42561a
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-05-03 14:04:52 +0200

    Adjusted recruitment-application page

--- frontend/src/components/recruitment-application.tsx
+++ frontend/src/components/recruitment-application.tsx
@@ -1,6 +1,8 @@
 import { Application } from '@/app/services/applications';
 import PDFViewer from './pdf-viewer';
 import dayjs from 'dayjs';
+import { FilePenIcon, FileTextIcon } from 'lucide-react';
+import { Separator } from './ui/separator';
 
 const RecruitmentApplication = ({
 	application,
@@ -11,14 +13,23 @@ const RecruitmentApplication = ({
 
 	return (
 		<div className="max-w-2xl m-auto">
-			<span>Sent {dayjs(createdAt).fromNow()}</span>
+			<div className="flex justify-end mb-2">
+				<span className="text-sm text-muted-foreground">
+					Created {dayjs(createdAt).fromNow()}
+				</span>
+			</div>
+			<Separator className="my-4" />
+			<h2 className="inline-flex items-center text-xl mb-4">
+				<FilePenIcon size={24} className="text-gray-500 mr-2" />
+				Introduction
+			</h2>
 			<p className="mb-8">{introduction}</p>
-			{cv && (
-				<>
-					<span className="inline-block">Your cv</span>
-					<PDFViewer href={`${import.meta.env.VITE_API_URI}/cv/${cv}`} />
-				</>
-			)}
+			<Separator className="my-4" />
+			<h2 className="inline-flex items-center text-xl mb-4">
+				<FileTextIcon size={24} className="text-gray-500 mr-2" />
+				CV
+			</h2>
+			{cv && <PDFViewer href={`${import.meta.env.VITE_API_URI}/cv/${cv}`} />}
 		</div>
 	);
 };
