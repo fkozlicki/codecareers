c67c7f7d8c27f938eb29a3b71ae691ce9723b26a
Filip Koźlicki <filip.kozlickii@gmail.com>
2024-07-21 23:48:38 +0200

    Changed app import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
-import app from './app.js';
+import app from '../api/index.js';
 
 const port = process.env.PORT || 3000;
 const server = createServer(app);
